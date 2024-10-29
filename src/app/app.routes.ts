cd8fa7bd860ab61b5d579477a378da34d97b0db0
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-10-29 14:27:34 -0300

    create page login/dashboard + template y scss inital login mobile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'dashboard', component:DashboardComponent, pathMatch:'full'},
+    {path:'login', component:LoginComponent, pathMatch:'full'},
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
